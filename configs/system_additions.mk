72ffdc07b1ff026d70df1b335c1512e2d31a4d65
Christian Oder <myself5@carbonrom.org>
2017-09-29 22:48:51 +0200

    vendor: use SDClang 4.0
    Change-Id: I3cafca38b0684078fe88365afeb18fc5d3c9fe33
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
 
 # Include SDCLANG definitions if it is requested and available
 ifeq ($(HOST_OS),linux)
-    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+    ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
         include vendor/pixeldust/sdclang/sdclang.mk
     endif
 endif
