50f772397cff6a5dbc9ac425e95007a27f40bdc5
spezi77 <spezi77@gmx.com>
2017-10-22 16:29:54 +0200

    Makefile: show meta information of the zip after build is completed
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -20,3 +20,9 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(PIXELDUST_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(PIXELDUST_TARGET_PACKAGE) > $(PIXELDUST_TARGET_PACKAGE).md5sum
 	$(hide) ./vendor/pixeldust/tools/pd.sh
+	@echo ""
+	@echo -e ${CL_CYN}"════════════════════════════════════════════════════════════════════════════════"${CL_RST}
+	@echo -e ${CL_CYN}"Package zip: "${CL_MAG} $(PIXELDUST_TARGET_PACKAGE)                                 ${CL_RST}
+	@echo -e ${CL_CYN}"Package md5: "${CL_MAG}" `cat $(PIXELDUST_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1`"${CL_RST}
+	@echo -e ${CL_CYN}"Package size:"${CL_MAG}" `du -h $(PIXELDUST_TARGET_PACKAGE) | cut -f 1`            "${CL_RST}
+	@echo -e ${CL_CYN}"════════════════════════════════════════════════════════════════════════════════"${CL_RST}
