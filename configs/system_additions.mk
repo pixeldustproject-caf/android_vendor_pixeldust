3335d51a0e61222ab0125e577afd59031d1fcadb
spezi77 <spezi7713@gmx.net>
2018-08-07 23:54:37 +0200

    Switch to sdclang 6.0.1
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
 
 # Include SDCLANG definitions if it is requested and available
 ifeq ($(HOST_OS),linux)
-    ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
+    ifneq ($(wildcard vendor/qcom/sdclang-6.0/),)
         include vendor/pixeldust/sdclang/sdclang.mk
     endif
 else
